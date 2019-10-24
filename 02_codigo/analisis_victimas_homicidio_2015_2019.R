262ed3dff5ec255d98966c11a4f8691cd866efa1
Sebastian Garrido <segasi@gmail.com>
2019-10-24 16:09:40 -0500

    Importar datos

--- 02_codigo/analisis_victimas_homicidio_2015_2019.R
+++ 02_codigo/analisis_victimas_homicidio_2015_2019.R
@@ -1,3 +1,9 @@
 ### Cargar paquetes, definir setup general y tema de gráficas ----
 source("02_codigo/paquetes_setup_tema.R")
 
+### Importar datos ----
+# Fuente: SNSP
+
+victimas <- 
+  read_excel("../../../../../10 recursos/datos/snsp/victimas/Estatal-V°ctimas - septiembre 2019.xlsx") %>% 
+  clean_names()
