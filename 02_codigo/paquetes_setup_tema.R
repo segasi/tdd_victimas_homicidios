5d91c085cc22e7bc611dbedbaed897eafe3aceb3
Sebastian Garrido <segasi@gmail.com>
2019-10-26 17:17:59 -0500

    Incluir paquete

--- 02_codigo/paquetes_setup_tema.R
+++ 02_codigo/paquetes_setup_tema.R
@@ -1,6 +1,6 @@
 ### Paquetes ----
 library(pacman)
-p_load(cowplot, ggtext, janitor, lubridate, psych, readxl, scales, tidyverse, treemapify, zoo)
+p_load(cowplot, forecast, ggtext, janitor, lubridate, psych, readxl, scales, sweep, tidyverse, timetk, treemapify, zoo)
 
 ### Setup general ----
 Sys.setlocale("LC_ALL", "es_ES.UTF-8") 
