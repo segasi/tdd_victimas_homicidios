31798c803249a760344d670a0e4376ed995de1f5
Sebastian Garrido <segasi@gmail.com>
2019-10-29 13:04:19 -0600

    Gráfica del número acumulado mensualmente de víctimas de homicidio doloso, por año, 2015-2019

--- 02_codigo/analisis_victimas_homicidio_2015_2019.R
+++ 02_codigo/analisis_victimas_homicidio_2015_2019.R
@@ -144,3 +144,44 @@ victimas %>%
   tema +
   theme(legend.position = "none") +
   ggsave("03_graficas/numero_mensual_victimas_homicidio_doloso_rango_y_500_3500.png", width = 14, height = 10, dpi = 200)
+
+
+### Gráfica del número acumulado mensualmente de víctimas de homicidio doloso, por año, 2015-2019 ----
+victimas %>% 
+  filter(fecha < as_date("2019-10-01"),
+         subtipo_de_delito == "Homicidio doloso") %>% 
+  group_by(fecha) %>% 
+  summarise(victimas_x_mes = sum(numero), 
+            mes = str_to_title(last(mes)),
+            año = mean(año), 
+            admin = last(admin), 
+            mes_num = mean(mes_num)) %>% 
+  ungroup() %>% 
+  arrange(fecha) %>% 
+  group_by(año) %>% 
+  mutate(victimas_acumuladas = cumsum(victimas_x_mes)) %>% 
+  ungroup() %>% 
+  select(fecha, año, mes, mes_num, victimas_x_mes, victimas_acumuladas, admin) %>% 
+  mutate(mes = fct_relevel(mes, "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"),
+         etiqueta_año = ifelse(año != 2019 & mes == "Diciembre", año, ""),
+         etiqueta_amlo = ifelse(año == 2019 & mes == "Septiembre", año, "")) %>% 
+  ggplot(aes(x = mes_num, y = victimas_acumuladas, group = año)) +
+  geom_line(aes(color = admin), size = 2, alpha = 1) +
+  geom_point(aes(color = admin), size = 1.6, alpha = 1) +
+  geom_text(aes(label = etiqueta_año), family = "Trebuchet MS Bold", hjust = -0.2, size = 6, color = "grey40") +
+  geom_text(aes(label = etiqueta_amlo), family = "Trebuchet MS Bold", vjust = -0.4, size = 6, color = "grey40") +
+  scale_y_continuous(labels = comma, breaks = seq(0, 40000, 2500)) +
+  scale_x_continuous(limits = c(1, 12.2), breaks = 1:12, labels = c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")) +
+  scale_color_manual(values = c("#af272f", "grey70")) +
+  labs(title = str_wrap(str_to_upper("Número acumulado mensualmente de víctimas de homicidio doloso, por año, 2015-2019"), width = 60),
+       subtitle = "Datos a septiembre de 2019",
+       x = "\n",
+       y = "Víctimas acumuladas\n",
+       color = NULL, 
+       caption = "@segasi / Fuente: SNSP\n") +
+  tema +
+  theme(legend.text = element_text(size = 15),
+        legend.background = element_rect(color = "#d3d3d380", fill = "#d3d3d330"),
+        legend.position = c(0.85, -0.12), 
+        legend.direction = "horizontal") +
+  ggsave("03_graficas/numero_acumulado_mensual_victimas_homicidio_doloso_por_año.png", width = 14.5, height = 10, dpi = 200)
